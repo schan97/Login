52f6cc3e1d9e59169946521709152d8be9cfaa50
Steven Chan <schan8525@gmail.com>
2020-04-20 16:31:15 -0400

    Create Login User Interface

--- Login/Form1.Designer.cs
+++ Login/Form1.Designer.cs
@@ -29,12 +29,124 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.Windows.Forms.Button btnLogin;
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.txtUsername = new System.Windows.Forms.TextBox();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			btnLogin = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Controls.Add(btnLogin);
+			this.panel1.Controls.Add(this.txtPassword);
+			this.panel1.Controls.Add(this.txtUsername);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Location = new System.Drawing.Point(107, 100);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(732, 378);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.RoyalBlue;
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(960, 209);
+			this.panel2.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Login.Properties.Resources.user;
+			this.pictureBox1.Location = new System.Drawing.Point(301, 16);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(144, 126);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			// 
+			// contextMenuStrip2
+			// 
+			this.contextMenuStrip2.Name = "contextMenuStrip2";
+			this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+			// 
+			// txtUsername
+			// 
+			this.txtUsername.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtUsername.Location = new System.Drawing.Point(220, 178);
+			this.txtUsername.Multiline = true;
+			this.txtUsername.Name = "txtUsername";
+			this.txtUsername.Size = new System.Drawing.Size(306, 37);
+			this.txtUsername.TabIndex = 1;
+			this.txtUsername.Text = "Username / Email";
+			this.txtUsername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtPassword.Location = new System.Drawing.Point(220, 241);
+			this.txtPassword.Multiline = true;
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.PasswordChar = '•';
+			this.txtPassword.Size = new System.Drawing.Size(306, 37);
+			this.txtPassword.TabIndex = 2;
+			this.txtPassword.Text = "Password";
+			this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// btnLogin
+			// 
+			btnLogin.BackColor = System.Drawing.Color.RoyalBlue;
+			btnLogin.FlatAppearance.BorderSize = 0;
+			btnLogin.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			btnLogin.Location = new System.Drawing.Point(301, 301);
+			btnLogin.Name = "btnLogin";
+			btnLogin.Size = new System.Drawing.Size(144, 44);
+			btnLogin.TabIndex = 3;
+			btnLogin.Text = "LOGIN";
+			btnLogin.UseVisualStyleBackColor = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(960, 582);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.panel2);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.TextBox txtPassword;
+		private System.Windows.Forms.TextBox txtUsername;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
 	}
 }
 
