78171c4e22d96d394e3190c5b23c8e375de5be26
Steven Chan <schan8525@gmail.com>
2020-04-20 18:14:01 -0400

    Refactor Form1.cs

--- Login/Form1.cs
+++ Login/Form1.cs
@@ -24,35 +24,25 @@ namespace Login
 		private void txtUsername_Enter(object sender, EventArgs e)
 		{
 			if(txtUsername.Text.Equals(@"Username / Email"))
-			{
 				txtUsername.Text = "";
-			}
-
 		}
 
 		private void txtUsername_Leave(object sender, EventArgs e)
 		{
 			if (txtUsername.Text.Equals(""))
-			{
 				txtUsername.Text = @"Username / Email";
-			}
-
 		}
 
 		private void txtPassword_Enter(object sender, EventArgs e)
 		{
 			if (txtPassword.Text.Equals("Password"))
-			{
 				txtPassword.Text = "";
-			}
 		}
 
 		private void txtPassword_Leave(object sender, EventArgs e)
 		{
 			if (txtPassword.Text.Equals(""))
-			{
 				txtPassword.Text = "Password";
-			}
 		}
 
 		private void btnLogin_Click(object sender, EventArgs e)
@@ -64,14 +54,10 @@ namespace Login
 			if(dr.Read())
 			{
 				if (txtUsername.Text.Equals(dr["username"].ToString()) && txtPassword.Text.Equals(dr["password"].ToString()))
-				{
 					MessageBox.Show("Login Successful", "Congrats", MessageBoxButtons.OK, MessageBoxIcon.Information);
-				}
 
 				else
-				{
 					MessageBox.Show("Invalid username or password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				}
 			}
 			con.Close();
 		}
